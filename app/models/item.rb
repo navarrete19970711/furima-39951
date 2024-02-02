14f664c9f2cf57a0933f15d0ca7f3a9df1bb60de
navarrete19970711 <mark.joseph.navarrete@gmail.com>
2024-02-02 18:53:02 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,27 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :comment
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },format: { with: /\A[0-9]+\z/ }
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
 
 
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :dekivety_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
   belongs_to :user
   has_one_attached :image
 end
