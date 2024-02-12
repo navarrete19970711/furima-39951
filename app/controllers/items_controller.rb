fa91a545cdeaa8703207319fe239ec8e210ac702
navarrete19970711 <mark.joseph.navarrete@gmail.com>
2024-02-12 16:46:24 +0900

    本番環境でのエラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to show
+      redirect_to item_path(@item)
     else
       render :edit, status: :unprocessable_entity
     end
