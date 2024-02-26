9e3aeb6303a58e2942f9ebed83e2e3ff897a6c05
navarrete19970711 <mark.joseph.navarrete@gmail.com>
2024-02-26 21:50:03 +0900

    修正④

--- spec/factories/delivery_addresses.rb
+++ spec/factories/delivery_addresses.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :purchase_delivery_address do
-    user_id { 1 }
-    item_id { 1 }
     postal_code { Faker::Number.decimal_part(digits: 3) + "-" + Faker::Number.decimal_part(digits: 4) }
     prefecture_id { Faker::Number.between(from: 1, to: 47) }
     municipality { Faker::Address.city }
